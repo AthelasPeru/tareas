b0d00600b1bf04526de7dc161cb086d88969a2ea
Pedro Baumann <ondoheer@gmail.com>
2015-02-18 11:02:32 -0500

    counter and limiter added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+	var charactersLeft = 3;
+
 	// función que acciona la selección de personajes
 	$(".character").on("click", function(){
 		
@@ -11,19 +13,26 @@ $(document).ready(function(){
 			// remove the class from the party
 			// TODO
 
-
+			charactersLeft += 1;
+			$(".characters-left").text(charactersLeft);
 
 		}
 		// si no tiene la clase active genera al PJ
-		else if(!$(this).hasClass("active")){
+		else if(!$(this).hasClass("active") && charactersLeft > 0){
 			var character = {}
 			character._class = $(this).find(".character-class").text();
-			character.stat1 = $(this).find(".stat1").text();
-			character.stat2 = $(this).find(".stat2").text();
-			character.stat3 = $(this).find(".stat3").text();
+			character.stat1 = $(this).find(".stat1").attr("value");
+			character.stat2 = $(this).find(".stat2").attr("value");
+			character.stat3 = $(this).find(".stat3").attr("value");
 			character.img = $(this).find("img").attr("src");
 			console.log(character);
-		};
+
+			charactersLeft -= 1;
+			$(".characters-left").text(charactersLeft);
+		} 
+		else if(charactersLeft <= 0){
+			alert("You have already selected 3 characters")
+		}
 
 		
 		
